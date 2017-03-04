e91696445dd31e0f8f4cb47d2b8f49c2c1e9ffbb
Michael James <michaeljames@nili.ca>
2017-03-04 17:57:11 -0500

    exercise5 completed 2.0

--- exercise5.rb
+++ exercise5.rb
@@ -1,8 +1,8 @@
 puts "temperature in Fahrenheit"
 tempf = gets.chomp.to_i
 
-def Calculate(tempf)
-  puts "temp in celcius equals #{(tempf - 32) * 5%9}"
+def calculate(tempf)
+  puts "temp in celcius equals #{(tempf - 32) * 5/9}"
 end
 
-Calculate(tempf)
+calculate(tempf)
